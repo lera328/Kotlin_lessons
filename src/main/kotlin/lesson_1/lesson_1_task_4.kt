922ef5e073f60c90a448d10b2581b445ce69559a
Lera <valeronfor@mail.ru>
2023-07-08 23:50:06 +0300

    KS-1-4

---
+++ src/main/kotlin/lesson_1/lesson_1_task_4.kt
@@ -0,0 +1,18 @@
+package lesson_1
+
+fun main() {
+    val length:Long = 40868600000
+    val age:Short = 27
+    val partOfDay:Float = 0.075F
+    val seconds:Short = 6480
+    val partOfYear:Double = 2.054794520547945E-4
+    val apogee:Int = 327000
+
+    println("Расстояние: $length")
+    println("Возраст: $age")
+    println("Часть дня: $partOfDay")
+    println("Кол-во секунд: $seconds")
+    println("Часть года: $partOfYear")
+    println("Апогей орбиты: $apogee")
+
+}
