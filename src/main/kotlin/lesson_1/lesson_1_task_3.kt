978d3d8e596183cec1532423d5cc5a5522d731a3
Lera <valeronfor@mail.ru>
2023-07-08 23:37:08 +0300

    KS-1-3

---
+++ src/main/kotlin/lesson_1/lesson_1_task_3.kt
@@ -0,0 +1,20 @@
+package lesson_1
+
+import java.time.Year
+
+fun main() {
+
+    val year:Int=1961
+
+    var hour:Int=9
+    var minute:Int=7
+
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour=10
+    minute=55
+    println("$hour:$minute")
+
+}
