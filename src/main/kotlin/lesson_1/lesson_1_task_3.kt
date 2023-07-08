bb753bc8b42cb7b55b1d19dce2278b0279e4b273
Lera <valeronfor@mail.ru>
2023-07-09 01:53:35 +0300

    KS-1-3.1

--- src/main/kotlin/lesson_1/lesson_1_task_3.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_3.kt
@@ -3,18 +3,15 @@ package lesson_1
 import java.time.Year
 
 fun main() {
-
-    val year:Int=1961
-
-    var hour:Int=9
-    var minute:Int=7
+    val year: Int = 1961
+    var hour: String = "09"
+    var minute: String = "07"
 
     println(year)
     println(hour)
     println(minute)
-
-    hour=10
-    minute=55
+    hour = "10"
+    minute = "55"
     println("$hour:$minute")
 
 }
