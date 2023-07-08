8a4f9c08acbeffbabaa5880343f330a06a04eeca
Lera <valeronfor@mail.ru>
2023-07-08 23:18:31 +0300

    KS-1-2

---
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -0,0 +1,17 @@
+package lesson_1
+
+fun main() {
+
+    var numOfOrders:Int = 75
+    val thankText:String = "Спасибо за покупку!"
+
+    println("Количество заказов: $numOfOrders")
+    println("Текст благодарности: $thankText")
+
+    var numOfWorkers:Int = 2000
+//    println("Количество работников: $numOfWorkers")
+    numOfWorkers-=1
+    println("Количество работников: $numOfWorkers")
+
+
+}
