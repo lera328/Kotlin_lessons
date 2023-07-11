86685a9c7b21718b41867865dd4e896e885f5505
Lera <valeronfor@mail.ru>
2023-07-11 15:08:05 +0300

    KS-2-5.1

--- src/main/kotlin/lesson_2/lesson_2_task_5.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_5.kt
@@ -1,11 +1,12 @@
 package lesson_2
 
+import kotlin.math.pow
+
 fun main() {
     val sum: Int = 70000
     val percentageRate: Double = 16.7
     val years: Double = 20.0
 
-    var result: Double = sum * Math.pow((1 + 0.01 * percentageRate), years)
-    result -= result % 0.001 //округление
-    println(result)
+    var result: Double = sum * (1 + 0.01 * percentageRate).pow(years)
+    println(String.format("%.3f",result))
 }
