ea6b1809b20753884c49cd126d34001ad18c12e1
Lera <valeronfor@mail.ru>
2023-07-11 18:43:07 +0300

    KS-2-1.1

--- src/main/kotlin/lesson_2/lesson_2_task_1.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_1.kt
@@ -1,12 +1,12 @@
 package lesson_2
 
 fun main() {
-    val values= arrayOf<Int>(3,4,3,5)
+    val values = arrayOf<Int>(3, 4, 3, 5)
 
-    var middleValue: Float=0f
+    var middleValue: Float = 0f
     for (element in values)
         middleValue += element
     middleValue /= values.size
-    println(String.format("%.2f",middleValue))
+    println(String.format("%.2f", middleValue))
 }
 
