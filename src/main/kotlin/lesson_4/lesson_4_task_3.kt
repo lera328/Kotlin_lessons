5d5e592af733f8adc97200cf2832f7327b48e8c9
Lera <valeronfor@mail.ru>
2023-07-16 19:23:37 +0300

    KS-4-3.1

--- src/main/kotlin/lesson_4/lesson_4_task_3.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_3.kt
@@ -1,14 +1,14 @@
 package lesson_4
 
 fun main() {
-    val weatherIsSunny: Boolean = true;
-    val tentIsOpen: Boolean = true;
-    val airHumidity: Int = 20;
+    val isWeatherSunny: Boolean = true
+    val tentIsOpen: Boolean = true
+    val airHumidity: Int = 20
     val timeOfTheYear: String = "зима"
 
     print(
         "Благоприятные ли условия сейчас для роста бобовых? ${
-            weatherIsSunny && tentIsOpen && (airHumidity == 20) && (timeOfTheYear != "зима")
+            isWeatherSunny && tentIsOpen && (airHumidity == 20) && (timeOfTheYear != "зима")
         }"
     )
 }
