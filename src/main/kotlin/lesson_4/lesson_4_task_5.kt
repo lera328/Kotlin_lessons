b24b385d623b6c9dc632f68e1ffacbc02ffa284d
Lera <valeronfor@mail.ru>
2023-07-16 19:31:33 +0300

    KS-4-5.1

--- src/main/kotlin/lesson_4/lesson_4_task_5.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_5.kt
@@ -4,14 +4,15 @@ const val MIN_TEAM_COUNT = 55
 const val MAX_TEAM_COUNT = 70
 const val MIN_FOOD_COUNT = 50
 fun main() {
-    print("Текущее состоянии корабля (1 - если корабль имеет повреждения, 0 - корабль исправен): ")
-    val isDefect = readln() == "0"
+    print("Текущее состоянии корабля ( true- если корабль имеет повреждения, false - корабль исправен): ")
+    val isDefect = readln().toBoolean()
+    println(isDefect)
     print("Количество человек в составе экипажа (только число): ")
     val teamCount = readln().toInt()
     print("Количество ящиков с провизией на борту (только число): ")
     val foodCount = readln().toInt()
-    print("Благоприятность метеоусловий (1 - если погода неблагоприятна, 0 - погода благоприятна): ")
-    val weather = readln() == "0"
+    print("Благоприятность метеоусловий (true - если погода неблагоприятна, false - погода благоприятна): ")
+    val weather = readln().toBoolean()
     if ((isDefect && (teamCount in MIN_TEAM_COUNT..MAX_TEAM_COUNT) && (foodCount > MIN_FOOD_COUNT))
         || ((teamCount == MAX_TEAM_COUNT) && weather && (foodCount > MIN_FOOD_COUNT))
     )
